51f6fa39ea65715c8998262b76b895f2423cbbe7
Kyle Paul <kylepaul1501@gmail.com>
2024-03-28 04:13:44 +0700

    train sucessfully with GPU

--- UNETR_LaPa/train.py
+++ UNETR_LaPa/train.py
@@ -1,6 +1,4 @@
 import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-
 import numpy as np
 import cv2
 from glob import glob
@@ -12,6 +10,8 @@ from sklearn.model_selection import train_test_split
 from patchify import patchify
 from unetr_2d import build_unetr_2d
 import matplotlib.pyplot as plt
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 # UNETR configuration
 cf = {}
@@ -29,7 +29,6 @@ cf["flat_patches_shape"] = (
     cf["num_patches"],
     cf["patch_size"] * cf["patch_size"] * cf["num_channels"]
 )
-
 
 def create_dir(path):
     if not os.path.exists(path):
@@ -85,47 +84,61 @@ def tf_dataset(X, Y, batch=2):
     ds = ds.map(tf_parse).batch(batch).prefetch(10)
     return ds
 
-if __name__ == "__main__":
-    # Seeding
-    np.random.seed(42)
-    tf.random.set_seed(42)
 
-    # Directory for storing files
-    create_dir("files")
+# Seeding
+np.random.seed(42)
+tf.random.set_seed(42)
 
-    # Hyperparameters
-    batch_size = 16
-    lr = 0.1
-    num_epochs = 3
-    model_path = os.path.join("files", "model.h5")
-    csv_path = os.path.join("files", "log.csv")
+# Directory for storing files
+create_dir("files")
 
-    # RGB Code and Classes
-    rgb_codes = [
-        [0, 0, 0], [0, 153, 255], [102, 255, 153], [0, 204, 153],
-        [255, 255, 102], [255, 255, 204], [255, 153, 0], [255, 102, 255],
-        [102, 0, 51], [255, 204, 255], [255, 0, 102]
-    ]
+# Hyperparameters
+batch_size = 16
+lr = 0.1
+num_epochs = 1
+model_path = os.path.join("files", "model.keras")
+csv_path = os.path.join("files", "log.csv")
 
-    classes = [
-        "background", "skin", "left eyebrow", "right eyebrow",
-        "left eye", "right eye", "nose", "upper lip", "inner mouth",
-        "lower lip", "hair"
-    ]
+# RGB Code and Classes
+rgb_codes = [
+    [0, 0, 0], [0, 153, 255], [102, 255, 153], [0, 204, 153],
+    [255, 255, 102], [255, 255, 204], [255, 153, 0], [255, 102, 255],
+    [102, 0, 51], [255, 204, 255], [255, 0, 102]
+]
 
-    dataset_path = "D:\Documents\GitHub\Triple-View-R-Net\data\LaPa"
-    (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_dataset(dataset_path)
+classes = [
+    "background", "skin", "left eyebrow", "right eyebrow",
+    "left eye", "right eye", "nose", "upper lip", "inner mouth",
+    "lower lip", "hair"
+]
 
-    print(f"Train: \t{len(train_x)} - {len(train_y)}")
-    print(f"Valid: \t{len(valid_x)} - {len(valid_y)}")
-    print(f"Test: \t{len(test_x)} - {len(test_y)}")
+dataset_path = "D:\Documents\GitHub\Triple-View-R-Net\data\LaPa"
+(train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_dataset(dataset_path)
 
-    train_dataset = tf_dataset(train_x, train_y, batch=batch_size)
-    valid_dataset = tf_dataset(valid_x, valid_y, batch=batch_size)
-    
-    model = build_unetr_2d(cf)
-    print(model.summary())
-    model.compile(
-        loss="categorical_crossentropy", 
-        optimizer=SGD(lr)
-    )
+print(f"Train: \t{len(train_x)} - {len(train_y)}")
+print(f"Valid: \t{len(valid_x)} - {len(valid_y)}")
+print(f"Test: \t{len(test_x)} - {len(test_y)}")
+
+train_dataset = tf_dataset(train_x, train_y, batch=batch_size)
+valid_dataset = tf_dataset(valid_x, valid_y, batch=batch_size)
+
+model = build_unetr_2d(cf)
+print(model.summary())
+model.compile(
+    loss="categorical_crossentropy", 
+    optimizer=SGD(lr)
+)
+
+callbacks = [
+    ModelCheckpoint(model_path, verbose=1, save_best_only=True),
+    ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, min_lr=1e-7, verbose=1),
+    CSVLogger(csv_path),
+    EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=False)
+]
+
+model.fit(
+    train_dataset,
+    epochs=num_epochs,
+    validation_data=valid_dataset,
+    callbacks=callbacks
+)
